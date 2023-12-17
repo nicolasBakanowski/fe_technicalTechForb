4612ddc6da1af411e5200986cd75df0a0b5ae4f7
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-17 04:27:15 -0300

    add template at component

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-nuevo-componente',
-  template: '<p>¡Hola! Soy el nuevo componente.</p>',
+  selector: 'login',
+  templateUrl: './login.component.html',
 })
 export class LoginComponent {}
