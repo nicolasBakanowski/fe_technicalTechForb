086893b23d314bb02b438f725d84c5293c339eb5
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-17 04:24:44 -0300

    add new component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { LoginComponent } from './components/login/login.component';
+
+// Define tus rutas
+export const routes: Routes = [
+  { path: '', component: LoginComponent }, // Ruta por defecto, muestra AppComponent
+];
---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-nuevo-componente',
+  template: '<p>¡Hola! Soy el nuevo componente.</p>',
+})
+export class LoginComponent {}
