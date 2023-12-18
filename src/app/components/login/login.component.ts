d47fad17cea79226dc5f9487c80d358e9049c7ba
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-18 03:22:54 -0300

    feat: new component login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,7 +1,30 @@
 import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth/auth.service';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'login',
   templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css'],
+  imports: [FormsModule],
+  standalone: true,
 })
-export class LoginComponent {}
+export class LoginComponent {
+  loginData = {
+    numberDocument: '',
+    password: '',
+  };
+
+  constructor(private authService: AuthService) {}
+
+  onSubmit() {
+    this.authService.login(this.loginData).subscribe({
+      next: (response) => {
+        console.log('Respuesta del servidor:', response);
+      },
+      error: (error) => {
+        console.error('Error en la solicitud:', error);
+      },
+    });
+  }
+}
