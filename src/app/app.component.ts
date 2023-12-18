ecb2d6a1336a8e9ec2c2a7978d5e34955555d2f6
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-18 03:23:03 -0300

    add imports in main app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,17 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from './services/auth/auth.service';
+import { ApiConfigurationService } from './services/apiConfigurations/apiConfiguration.service';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HttpClientModule],
+  providers: [ApiConfigurationService, AuthService],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'fe_technicalTest';
