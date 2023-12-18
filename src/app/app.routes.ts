a5be50babba040c500906b03f90370662a0cb72d
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-18 06:15:30 -0300

    feat: add dashboard component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,9 @@
 import { Routes } from '@angular/router';
 
 import { LoginComponent } from './components/login/login.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-// Define tus rutas
 export const routes: Routes = [
-  { path: '', component: LoginComponent }, // Ruta por defecto, muestra AppComponent
+  { path: '', component: LoginComponent },
+  { path: 'dashboard', component: DashboardComponent },
 ];
