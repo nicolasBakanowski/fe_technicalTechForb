852d715fd36fd9d3687db1e8b5ac498502a0e48d
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-21 06:35:48 -0300

    delete useless import

--- src/app/services/menu/menu.service.ts
+++ src/app/services/menu/menu.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { ApiConfigurationService } from '../apiConfigurations/apiConfiguration.service';
 import { AuthService } from '../auth/auth.service';
 
