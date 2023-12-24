79dbd88f13ce9b1dd6b823825bde8de247409129
nicolasBakanowski <nicolasbakanowski@gmail.com>
2023-12-24 06:30:40 -0300

    add index page

--- src/app/components/index/index.component.ts
+++ src/app/components/index/index.component.ts
@@ -1,11 +1,21 @@
 import { Component } from '@angular/core';
 import { LoginComponent } from '../login/login.component';
-
+import { RegisterComponent } from '../register/register.component';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-index',
   standalone: true,
-  imports: [LoginComponent],
+  imports: [LoginComponent, RegisterComponent, CommonModule],
   templateUrl: './index.component.html',
   styleUrl: './index.component.css',
 })
-export class IndexComponent {}
+export class IndexComponent {
+  formView: boolean = true;
+  InLogin() {
+    this.formView = false;
+  }
+
+  InRegister() {
+    this.formView = true;
+  }
+}
